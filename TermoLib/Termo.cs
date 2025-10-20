cb1ae098cadf79a63d8b979cd3a33d9f80867629
Marinr <Marinr@DESKTOP-8TAE00T>
2025-10-20 20:04:32 -0300

    correção das cores do tabuleiro e teclado, assim como a lógica de hiearquica de cores em palavras com letras repetidas

--- TermoLib/Termo.cs
+++ TermoLib/Termo.cs
@@ -1,4 +1,9 @@
-﻿namespace TermoLib
+﻿using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TermoLib
 {
     public class Letra 
     {
@@ -19,12 +24,14 @@
         public List<List<Letra>> tabuleiro;
         public Dictionary<char, char> teclado;
         public int palavraAtual;
+        public int cont;
         public bool JogoFinalizado;
 
         public Termo() {
             CarregaPalavras("Palavras.txt");
             SorteiaPalavra();
             palavraAtual = 1;
+            cont = 0;
             JogoFinalizado = false;
             tabuleiro = new List<List<Letra>>();
             teclado = new Dictionary<char, char>();
@@ -56,7 +63,7 @@
 
         public void ChecaPalavra(string palavra)
         {
-            if (palavra == palavraSorteada)
+            if (palavra == palavraSorteada || cont == 25)
             {
                 JogoFinalizado = true;
             }
@@ -65,10 +72,20 @@
 
             if(vp == true)
             {
+               
                 var palavraTabuleiro = new List<Letra>();
                 char cor;
                 for (int i = 0; i < palavra.Length; i++)
                 {
+                    if (palavraSorteada.Distinct().Count() > palavraSorteada.Length)//tem letras repitidas
+                    {
+                        var letraRepetida = palavraSorteada.GroupBy(c => c)
+                           .Where(g => g.Count() > 1)
+                           .FirstOrDefault();
+
+                        
+                    }
+
                     if (palavra[i] == palavraSorteada[i])
                     {
                         cor = 'V';
@@ -88,8 +105,89 @@
                 palavraAtual++;
             }
         }
+        public void ChecaPalavraV2(string palavra)
+        {
+            if (palavra == palavraSorteada)
+            {
+                JogoFinalizado = true;
+                var palavraVencedora = new List<Letra>();
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    palavraVencedora.Add(new Letra(palavra[i], 'V'));
+                    teclado[palavra[i]] = 'V';
+                }
+                tabuleiro.Add(palavraVencedora);
+                palavraAtual++;
+                return;
+            }
 
-        public bool validaPalavra(string palavra)
+            if (cont == 30)
+            {
+                JogoFinalizado = true;
+            }
+
+            bool vp = validaPalavra(palavra);
+
+            if (vp == true)
+            {
+                var cores = new char[palavra.Length];
+                var palavraSorteadaTemp = new StringBuilder(palavraSorteada);
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (palavra[i] == palavraSorteada[i])
+                    {
+                        cores[i] = 'V';
+                        palavraSorteadaTemp[i] = '_';
+                    }
+                }
+
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    if (cores[i] == 'V')
+                    {
+                        continue;
+                    }
+
+                    int indexNaSorteada = palavraSorteadaTemp.ToString().IndexOf(palavra[i]);
+
+                    if (indexNaSorteada != -1)
+                    {
+                        cores[i] = 'A';
+                        palavraSorteadaTemp[indexNaSorteada] = '_';
+                    }
+                    else
+                    {
+                        cores[i] = 'P';
+                    }
+                }
+                var palavraTabuleiro = new List<Letra>();
+                for (int i = 0; i < palavra.Length; i++)
+                {
+                    palavraTabuleiro.Add(new Letra(palavra[i], cores[i]));
+
+                    char letra = palavra[i];
+                    char corNova = cores[i];
+
+                    if (!teclado.ContainsKey(letra) || corNova == 'V')
+                    {
+                        teclado[letra] = corNova;
+                    }
+                    else if (corNova == 'A' && teclado[letra] != 'V')
+                    {
+                        teclado[letra] = corNova;
+                    }
+                    else if (!teclado.ContainsKey(letra))
+                    {
+                        teclado[letra] = corNova;
+                    }
+                }
+
+                tabuleiro.Add(palavraTabuleiro);
+                palavraAtual++;
+        }
+    }
+    public bool validaPalavra(string palavra)
         {
             bool vp = false;
             foreach (string p in palavras)
